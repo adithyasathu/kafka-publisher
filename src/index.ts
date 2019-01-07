f3e991204bcc3073c848c0818c064f732d487fa1
Adithya Sathu <adithya.sathu@healthdirect.org.au>
2019-01-07 11:03:26 +1100

    included git ignore file and replaced console log

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import * as config from "config";
 import { Worker } from "./worker";
 import {IWorker} from "./interfaces";
 import {Publisher} from "./publisher";
+import * as Logger from "simple-node-logger";
 const kafkaConfig: any = config.get("worker.kafka");
+export const log = Logger.createSimpleLogger();
 
 export async function fakeSales(num)  {
     let worker: IWorker = null;
@@ -10,7 +12,7 @@ export async function fakeSales(num)  {
         worker = new Worker(new Publisher(kafkaConfig), kafkaConfig.topic);
         await worker.start(num);
     } catch (e) {
-        console.error(e);
+        log.error(e);
     } finally {
         worker.stop();
     }
